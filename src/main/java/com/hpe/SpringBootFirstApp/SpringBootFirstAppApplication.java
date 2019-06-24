96cb89b34b7d641419d0b6ffd1fa8a0cb1084f33
Mohan <mohanvijay1991@gmail.com>
2019-06-25 01:19:47 +0530

    Added Controller and JSP and CSS to Create sample spring boot application

--- src/main/java/com/hpe/SpringBootFirstApp/SpringBootFirstAppApplication.java
+++ src/main/java/com/hpe/SpringBootFirstApp/SpringBootFirstAppApplication.java
@@ -2,12 +2,20 @@ package com.hpe.SpringBootFirstApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootFirstAppApplication {
+public class SpringBootFirstAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirstAppApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// TODO Auto-generated method stubapp
+		return application.sources(SpringBootFirstAppApplication.class);
 	}
 
 }
